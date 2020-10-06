bfe4cd364128a6020725ded49c4d5f2e2cd965d7
Corban Swain <CorbanSwain@gmail.com>
2020-10-06 11:26:34 -0400

    working parser code

--- pam.py
+++ pam.py
@@ -19,6 +19,13 @@ def iseven(x):
     return (x % 2) == 0
 
 
+indent_str = ' ' * 4
+
+
+def indent(string):
+    return indent_str + (('\n' + indent_str).join(string.splitlines()))
+
+
 # to bytes
 
 def uint_to_bytes(x,
@@ -153,12 +160,11 @@ class Token(object):
         return '{:s}({:17s}, {:})'.format(
             self.__class__.__name__,
             self.type.name,
-            self.type.value_string(self.value)
-        )
+            self.type.value_string(self.value))
 
-    @staticmethod
-    def print_list(tokens):
-        [print(f'{i:03d} | {t}') for i, t in enumerate(tokens)]
+
+def print_token_list(tokens):
+    [print(f'{i:03d} | {t}') for i, t in enumerate(tokens)]
 
 
 class PAM(object):
@@ -176,7 +182,14 @@ class PAM(object):
         return bytes().join(bytes_list)
 
     def __repr__(self):
-        pass  # TODO
+        return '''{:s}(name='{:s}',
+    shape={}, 
+    slices=[{:s}])'''.format(
+            self.__class__.__name__,
+            self.name,
+            self.shape,
+            ('' if not self.slices else
+             ('\n' + indent(',\n'.join(indent(f'{s}') for s in self.slices)))))
 
     def write_binary(self, filepath):
         with open(filepath, 'wb') as fle:
@@ -188,7 +201,7 @@ class PAM(object):
     @classmethod
     def from_binary(cls, byts):
         tokens = cls._lex_binary(byts)
-        pam = cls.__init__()
+        pam = cls()
 
         _token = tokens.pop(0)
         assert _token.type is TokenType.STRING
@@ -243,6 +256,9 @@ class PAMSlice(object):
         self.features = [] if features is None else features
         self.unknown_order = unknown_order
 
+        self._unknown_bytes_raw = None
+        self._tail_raw = None
+
     def __bytes__(self):
         tail_bytes_list = (
             [self._unknown_bytes,
@@ -258,7 +274,14 @@ class PAMSlice(object):
                 + tail_bytes + self.footer_bytes)
 
     def __repr__(self):
-        pass  # TODO
+        return '''{:s}(
+    features=[{:s}], 
+    unknown_order={})'''.format(
+            self.__class__.__name__,
+            ('' if not self.features else
+             ('\n' + indent(',\n'.join(indent(f'{s}')
+                                       for s in self.features)))),
+            self.unknown_order)
 
     @classmethod
     def from_token(cls, token):
@@ -276,37 +299,62 @@ class PAMSlice(object):
     def _from_binary(cls, byts):
         tokens = cls._lex_binary(byts)
 
+        pam_slice = cls()
+
+        _next_token = tokens.pop(0)
+        assert _next_token.type is TokenType.BYTES
+        pam_slice._unknown_bytes_raw = _next_token.value
+        pam_slice._tail_raw = byts[4:]
+
+        _next_token = tokens.pop(0)
+        assert _next_token.type is TokenType.UINT_32
+        assert iseven(_next_token.value)
+        pam_slice.unknown_order = _next_token.value // 2
+
+        while tokens:
+            pam_slice.features.append(PAMFeature.from_token(tokens.pop(0)))
+
+        return pam_slice
+
     @staticmethod
     def _lex_binary(byts):
         byte_arr = bytearray(byts)
         tokens = []
 
-        _ = pop_word(byte_arr)
-
         tokens.append(Token(TokenType.BYTES, pop_word(byte_arr)))
+
+        _ = pop_word(byte_arr)
 
         next_len = pop_uint32(byte_arr)
         tokens.append(Token(TokenType.UINT_32, next_len))
         _ = pop_bytes(byte_arr, next_len * 2)
 
+        feature_bytes_list = []
+        feature_bytes = pop_word(byte_arr)
         while True:
             next_word = peek_word(byte_arr)
-            feature_bytes = []
 
             if (next_word == PAMSlice.end_of_features_bytes
                     and len(byte_arr) == byte_word_length):
                 if feature_bytes:
+                    feature_bytes_list.append(feature_bytes)
                     tokens.append(Token(TokenType.PAM_FEATURE_BYTES,
-                                        feature_bytes))
+                                        feature_bytes_list))
 
                 _ = pop_word(byte_arr)
                 break
-            elif next_word == PAMSlice.separator_bytes
+
+            elif next_word.endswith(PAMFeature.header_tail_bytes):
+                feature_bytes_list.append(feature_bytes)
+                tokens.append(Token(TokenType.PAM_FEATURE_BYTES,
+                                    feature_bytes_list))
+                feature_bytes_list = []
+                feature_bytes = pop_word(byte_arr)
+
+            else:
+                feature_bytes += pop_word(byte_arr)
 
         return tokens
-
-
-
 
     @property
     def unknown_order(self):
@@ -321,7 +369,9 @@ class PAMSlice(object):
     def _unknown_bytes(self):
         unknown_bytes = None
 
-        if self._isempty:
+        if self._unknown_bytes_raw:
+            unknown_bytes = self._unknown_bytes_raw
+        elif self._isempty:
             unknown_bytes = bytes([14, 129, 0, 108])
         elif len(self.features) == 1:
             feature_type = type(self.features[0])
@@ -349,19 +399,72 @@ class PAMSlice(object):
 
 
 class PAMFeature(object):
-    header_bytes_tail = bytes([0, 0, 16])
+    header_tail_bytes = bytes([0, 0, 16])
     header_bytes = None
     tail_bytes = None
 
     def __init__(self, header_bytes=None, tail_bytes=None):
         if header_bytes is not None:
-            self.header_bytes = header_bytes
+            self.header_bytes = bytes(header_bytes)
 
         if tail_bytes is not None:
-            self.tail_bytes = tail_bytes
+            self.tail_bytes = bytes(tail_bytes)
 
     def __bytes__(self):
         return self.header_bytes + self.tail_bytes
+
+    def __repr__(self):
+        return '''{:s}(header_bytes={}, tail_bytes={:s})'''.format(
+            self.__class__.__name__,
+            (None if self.header_bytes is None else
+             [b for b in self.header_bytes]),
+            (repr(None) if self.tail_bytes is None else
+             ('[' + ', '.join(f'{b}' for b in self.tail_bytes[:4]) + ', ...]'
+              if len(self.tail_bytes) > 4
+              else f'{[b for b in self.tail_bytes]}')))
+
+    @classmethod
+    def from_token(cls, token):
+        assert token.type is TokenType.PAM_FEATURE_BYTES
+
+        if not len(token.value) == 1:
+            raise NotImplementedError
+
+        byts = token.value[0]
+        tokens = cls._lex_binary(byts)
+
+        _next_token = tokens.pop(0)
+        assert _next_token.type is TokenType.BYTES
+        header_bytes = _next_token.value
+
+        _next_token = tokens.pop(0)
+        assert _next_token.type is TokenType.BYTES
+        tail_bytes = _next_token.value
+
+        feature_class = cls.header_bytes_to_class(header_bytes)
+        return feature_class.from_binary(header_bytes, tail_bytes)
+
+    @classmethod
+    def from_binary(cls, header_bytes, tail_bytes):
+        return cls(header_bytes, tail_bytes)
+
+    @staticmethod
+    def _lex_binary(byts):
+        byte_arr = bytearray(byts)
+        tokens = [Token(TokenType.BYTES, pop_word(byte_arr)),
+                  Token(TokenType.BYTES, pop_bytes(byte_arr, len(byte_arr)))]
+        return tokens
+
+    @staticmethod
+    def header_bytes_to_class(header_bytes):
+        if header_bytes == RectanglePAMFeature.header_bytes:
+            return RectanglePAMFeature
+        elif header_bytes == EmptyPAMFeature.header_bytes:
+            return EmptyPAMFeature
+        elif header_bytes == InvertPAMFeature.header_bytes:
+            return InvertPAMFeature
+        else:
+            return PAMFeature
 
     @staticmethod
     def point_to_bytes(point):
@@ -371,8 +474,7 @@ class PAMFeature(object):
 
 class RectanglePAMFeature(PAMFeature):
     header_bytes = bytes([1, 0, 0, 16])
-    separator_bytes = bytes([4, 0, 0, 0,
-                             0, 64, 0, 0])
+    separator_bytes = bytes([0, 64, 0, 0])
     footer_bytes = bytes([0, 1, 1, 129])
 
     def __init__(self, top_left_point=None, size=None):
@@ -394,6 +496,81 @@ class RectanglePAMFeature(PAMFeature):
             self.__class__.__name__,
             top_left_point_repr,
             size_repr)
+
+    @classmethod
+    def from_binary(cls, _, tail_bytes):
+        tokens = cls._lex_binary(tail_bytes)
+
+        _next_token = tokens.pop(0)
+        assert _next_token.type == TokenType.UINT_32, \
+            f'Error while parsing binary. Expected next token to be UINT_32.'
+        num_points = _next_token.value
+
+        if num_points == 4:
+            point_list = []
+            for _ in range(num_points):
+                _next_token = tokens.pop(0)
+                assert _next_token.type == TokenType.UINT_16
+                x_val = _next_token.value
+
+                _next_token = tokens.pop(0)
+                assert _next_token.type == TokenType.UINT_16
+                y_val = _next_token.value
+
+                point_list.append(np.array([x_val, y_val]))
+
+            # TODO - add validity checks for rectangle
+
+            top_left = point_list[0]
+            size = point_list[2] - top_left
+
+            if (size == [1, 1]).all():
+                return PointPAMFeature(point=top_left)
+            else:
+                return cls(top_left_point=top_left, size=size)
+        else:
+            print(f'Parsing failed. Expected 4 points, but got {num_points} '
+                  f'points instead. Returning general PAMFeature instance.')
+            return PAMFeature(cls.header_bytes, tail_bytes)
+
+    @classmethod
+    def _lex_binary(cls, byts):
+        byte_arr = bytearray(byts)
+        tokens = []
+
+        _next_len = pop_uint32(byte_arr)
+        err_header = 'Error while lexing binary to generate tokens for a ' \
+                     'PAMFeature.'
+        assert len(byte_arr) == _next_len, err_header + \
+            f'Expected len of {_next_len:d} bytes; found len of '\
+            f'{len(byte_arr):d} bytes instead.'
+
+        _next_word = pop_word(byte_arr)
+        assert _next_word == PAMSlice.separator_bytes  # TODO - add message
+
+        _next_len = pop_uint32(byte_arr)
+        tokens.append(Token(TokenType.UINT_32, _next_len))
+
+        _next_word = pop_word(byte_arr)
+        assert _next_word == cls.separator_bytes, err_header + \
+            f'Expected next bytes to be {[b for b in cls.separator_bytes]}; ' \
+            f'instead got {[b for b in _next_word]}.'
+
+        for _ in range(_next_len):
+            tokens += [Token(TokenType.UINT_16, pop_uint16(byte_arr))
+                       for _ in range(2)]
+
+        _next_word = peek_word(byte_arr)
+        if _next_word == cls.footer_bytes:
+            _ = pop_word(byte_arr)
+        elif len(_next_word) == 0:
+            pass
+        else:
+            # FIXME - use function for byte arr conversion
+            raise AssertionError(f'Expected end of binary, however bytes '
+                                 f'remain: {[b for b in byte_arr]}.')
+
+        return tokens
 
     @property
     def size(self):
@@ -431,9 +608,14 @@ class RectanglePAMFeature(PAMFeature):
                 self.bottom_left_point]
 
     @property
+    def num_points(self):
+        return len(self._all_points)
+
+    @property
     def tail_bytes(self):
         tail_bytes_list = (
                 [PAMSlice.separator_bytes,
+                 uint32_to_bytes(self.num_points),
                  self.separator_bytes]
                 + [self.point_to_bytes(x) for x in self._all_points]
                 + [self.footer_bytes])
@@ -462,22 +644,26 @@ class EmptyPAMFeature(PAMFeature):
     header_bytes = bytes([2, 0, 0, 16])
     tail_bytes = bytes()
 
-    def __init__(self):
-        super().__init__()
-
     def __repr__(self):
         return self.__class__.__name__ + '()'
+
+    @classmethod
+    def from_binary(cls, _, tail_bytes):
+        assert len(tail_bytes) == 0
+        return cls()
 
 
 class InvertPAMFeature(PAMFeature):
     header_bytes = bytes([0, 0, 0, 16])
     tail_bytes = bytes(([0] * 8) + ([0, 0, 0, 67] * 2))
 
-    def __init__(self):
-        super().__init__()
-
     def __repr__(self):
         return self.__class__.__name__ + '()'
+
+    @classmethod
+    def from_binary(cls, _, tail_bytes):
+        assert tail_bytes == cls.tail_bytes
+        return cls()
 
 
 def main():
@@ -491,11 +677,26 @@ def main():
     filepath = os.path.join('data', 'gen_test.pam')
     pam.write_binary(filepath)
 
-    filepath = os.path.join('data', 'test_z_mask_05.pam')
+    filepath = os.path.join('data', 'test_z_mask_07.pam')
     with open(filepath, 'rb') as fle:
         byts = fle.read()
     tokens = PAM._lex_binary(byts)
-    Token.print_list(tokens)
+    print('\nPAM Tokens')
+    print_token_list(tokens)
+
+    slice_tokens = PAMSlice._lex_binary(tokens[12].value[0])
+    print('\nSlice Tokens:')
+    print_token_list(slice_tokens)
+
+    # feature_bytes = slice_tokens[3].value[0][4:]
+    # feature_tokens = RectanglePAMFeature._lex_binary(feature_bytes)
+    # print('\nFeature Tokens:')
+    # print_token_list(feature_tokens)
+    # print(RectanglePAMFeature.from_binary(bytes(), feature_bytes))
+
+    pam_2 = PAM.from_binary(byts)
+    print(pam_2)
+
 
 
 if __name__ == '__main__':
