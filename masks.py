9134b83d2cc04587eca160444d02721ef7c292d0
Corban Swain <CorbanSwain@gmail.com>
2020-10-22 11:39:13 -0400

    work on masks handler

--- masks.py
+++ masks.py
@@ -4,25 +4,29 @@
 import numpy as np
 
 
-class Mask:
+class Mask(object):
 
     def __init__(self, mask_data):
         self.mask_data = mask_data
 
-    def get_chunks(self):
+    def get_chunks(self, do_points_only=True):
         chunks = []
 
         for z_loc in range(self.mask_data.shape[2]):
-            locs = np.where(self.mask_data[:, :, z_loc])
-            chunk_sublist = []
-            for x_loc, y_loc in zip(locs):
-                new_chunk = Chunk(
-                    xy_loc=(x_loc, y_loc),
-                    z_loc=z_loc,
-                    xy_size=(1, 1),
-                    value=self.mask_data[x_loc, y_loc, z_loc])
-                chunk_sublist.append(new_chunk)
-            chunks.append(chunk_sublist)
+            if do_points_only:
+                locs = np.where(self.mask_data[:, :, z_loc])
+                chunk_sublist = []
+                for x_loc, y_loc in zip(locs):
+                    new_chunk = Chunk(
+                        xy_loc=(x_loc, y_loc),
+                        z_loc=z_loc,
+                        xy_size=(1, 1),
+                        value=self.mask_data[x_loc, y_loc, z_loc])
+                    chunk_sublist.append(new_chunk)
+                chunks.append(chunk_sublist)
+
+            else:
+                raise NotImplementedError
 
         return chunks
 
@@ -40,10 +44,22 @@ class Mask:
         else:
             raise NotImplementedError
 
-class Chunk:
-    def __init__(self, xy_loc, z_loc, xy_size, value, label):
+
+class ChunkList(object):
+    def __init__(self, chunks=None, label=None):
+        self.chunks = [] if chunks is None else chunks
+        self.label = label
+
+    def __iter__(self):
+
+
+
+class Chunk(object):
+    def __init__(self, xy_loc, z_loc, xy_size, value=1, label=None):
         self.z_loc = z_loc
         self.xy_loc = xy_loc
         self.xy_size = xy_size
         self.value = value
         self.label = label
+
+
